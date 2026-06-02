989d44501b099824866e7fe7d54f49456258828f
perttunevala <perttunevala@gmail.com>
2026-06-02 16:29:03 +0300

    Update error message for invalid token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,7 +14,7 @@ const authMiddleware = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Invalid or expired token' });
+    return res.status(403).json({ message: 'Invalid token' });
   }
 };
 
